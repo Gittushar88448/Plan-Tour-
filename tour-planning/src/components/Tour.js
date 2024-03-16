6cc6cbb258008bd0b9c243b3982ad0a2dbc5f19f
Tushar <kmrtushar01@gmail.com>
2024-03-16 13:57:16 +0530

    defining tour component

--- tour-planning/src/components/Tour.js
+++ tour-planning/src/components/Tour.js
@@ -1,6 +1,21 @@
+import Card from "./Card";
 
+function Tour({tours , clickHandler}){
 
-function Tour(){
+    return(
+        <div>
+            <div>
+                <h1>Plan Tour With Tushar</h1>
+            </div>
+            <div>
+                {
+                tours.map((tour)=> {
+                    return <Card key={tour.id} {...tour} clickHandler={clickHandler}></Card>
+                })
+            }
+            </div>
+        </div>
+    )
 
 }
 export default Tour;
