a4a5fc1f3ffdd512b0c22d72aa84a48fc4d97104
Tushar <kmrtushar01@gmail.com>
2024-03-16 14:06:39 +0530

    adding className to tour

--- tour-planning/src/components/Tour.js
+++ tour-planning/src/components/Tour.js
@@ -3,16 +3,18 @@ import Card from "./Card";
 function Tour({tours , clickHandler}){
 
     return(
-        <div>
+        <div className="container">
             <div>
-                <h1>Plan Tour With Tushar</h1>
+                <h1 className="top-heading">Plan Tour With Tushar</h1>
             </div>
-            <div>
+
+            <div className="cards">
                 {
                 tours.map((tour)=> {
                     return <Card key={tour.id} {...tour} clickHandler={clickHandler}></Card>
                 })
             }
+            
             </div>
         </div>
     )
