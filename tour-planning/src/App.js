af20b30921596e94c04abe6dbedf14105df9c43e
Tushar <kmrtushar01@gmail.com>
2024-03-16 13:34:41 +0530

    adding tour component

--- tour-planning/src/App.js
+++ tour-planning/src/App.js
@@ -1,14 +1,25 @@
 import { useState } from 'react';
-import './App.css';
 import Tour from './components/Tour';
 import data from './data';
 
 function App() {
+
   const [tours , setTours] = useState(data);
+
+  if(tours.length === 0) {
+    return (
+        <div className="refresh">
+          <h2>No Tours Left</h2>
+          <button className="btn-white" onClick={() => setTours(data)}>
+            Refresh
+          </button>
+        </div>
+    );
+  }
   return (
     <div className="App">
 
-      <Tour tour={tours}></Tour>
+      <Tour tours={tours}></Tour>
 
     </div>
   );
